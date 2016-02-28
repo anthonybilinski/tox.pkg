50de979c29ec103fda164020400877b1a6e99214
Anton Batenev <antonbatenev@yandex.ru>
2016-02-28 14:37:53 +0300

    [tox-bootstrapd] No sed magic more - move init/systemd scripts to own repo

--- tox-libtoxcore/build.sh
+++ tox-libtoxcore/build.sh
@@ -31,7 +31,11 @@ rm -f "${BUILD_DIR}/${PACKAGE_NAME}_"*.changes
 rm -f "${BUILD_DIR}/${PACKAGE_NAME}_"*.build
 
 cp -rf "${BASE}/debian" "${SOURCE_DIR}/debian"
-cp -f "${BASE}/tox-bootstrapd.conf" "${SOURCE_DIR}/other/bootstrap_daemon/tox-bootstrapd.conf"
+
+cp -f "${BASE}/tox-bootstrapd.conf"    "${SOURCE_DIR}/other/bootstrap_daemon/tox-bootstrapd.conf"
+cp -f "${BASE}/tox-bootstrapd.service" "${SOURCE_DIR}/other/bootstrap_daemon/tox-bootstrapd.service"
+cp -f "${BASE}/tox-bootstrapd.sh"      "${SOURCE_DIR}/other/bootstrap_daemon/tox-bootstrapd.sh"
+cp -f "${BASE}/tox-bootstrapd.temp"    "${SOURCE_DIR}/other/bootstrap_daemon/tox-bootstrapd.temp"
 
 cd "${SOURCE_DIR}"
 
--- tox-libtoxcore/debian/patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-for PATCH in $(cat debian/patches/series)
-do
-	patch -p0 < debian/patches/${PATCH}
-done
